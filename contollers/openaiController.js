a68c78e47417d6720440207d5bbf89e8df6d1cca
oluwaseun <adunmofestus@gmail.com>
2025-08-25 21:56:48 +0100

    generating Metadata base on the title provided

--- contollers/openaiController.js
+++ contollers/openaiController.js
@@ -11,8 +11,19 @@ const generateMeta = async (title) => {
         ],
         max_tokens: 100
     })
+    const tags = await openai.chat.completions.create({
+        model: "gpt-4.1-nano",
+        messages: [
+            {
+                role: "user",
+                content: `Come up with 10 keywords for Youtube video called: ${title}`
+            }
+        ],
+        max_tokens: 100
+    })
 
     console.log(description.choices[0].message.content)
+    console.log(tags.choices[0].message.content)
 }
 
 module.exports = { generateMeta }
